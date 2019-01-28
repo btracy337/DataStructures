345ba31d4e82eb0ebfdc24360bd3c2936a77b23e
Bryan Tracy <btracy337@gmail.com>
2019-01-28 09:17:34 -0700

    Added node in the model package. Also added Controller, Runner, and their associated methods.

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,15 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Tracy, Bryan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    cout << "Welcome to the data structures app" << endl;
+    
+}
---
+++ DataStructures/Model/Node.hpp
@@ -0,0 +1,26 @@
+//
+//  Node.hpp
+//  DataStructures
+//
+//  Created by Tracy, Bryan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <assert.h>
+
+template <class Type>
+class Node
+{
+private:
+    Type data;
+public:
+    Node(Type data);
+    Type getData();
+    void setData(Type data);
+};
+
+
+#endif /* Node_hpp */
