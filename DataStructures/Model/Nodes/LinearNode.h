c68c851f60d4aaed0e6bb9d92459d9ca41eca368
Bryan Tracy <btracy337@gmail.com>
2019-01-30 09:44:16 -0700

    Added Array class for an object oriented array

---
+++ DataStructures/Model/Nodes/LinearNode.h
@@ -0,0 +1,50 @@
+//
+//  LinearNode.h
+//  DataStructures
+//
+//  Created by Tracy, Bryan on 1/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_h
+#define LinearNode_h
+template <class Type>
+class LinearNode : public Node<Type>
+{
+private:
+    LinearNode<Type> * next;
+public:
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    LinearNode<Type> * getNext();
+    void setNext(LinearNode<Type> * next);
+};
+
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node()
+{
+    
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this -> next;
+}
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNext()
+{
+    return this-> next;
+}
+template <class Type>
+void LinearNode<Type> :: setNext(LinearNode<Type> * next)
+{
+    this -> next = next;
+}
+
+#endif /* LinearNode_h */
