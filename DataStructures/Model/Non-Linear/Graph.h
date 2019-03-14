8ac8eb6fec7bb55650cd961dff67a4aa5b641d44
Bryan Tracy <btracy337@gmail.com>
2019-03-14 09:54:02 -0600

    Added Graph Structure and associated methods.

---
+++ DataStructures/Model/Non-Linear/Graph.h
@@ -0,0 +1,113 @@
+//
+//  Graph.h
+//  DataStructures
+//
+//  Created by Tracy, Bryan on 3/14/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph{
+private:
+    static const int MAXIMUM = 50;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTravseral(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    
+    void addVertex(const Type& value);
+    
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    Type& operator[] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std::set<int> neighbors(int vertex) const;
+    
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+}
+template <class Type>
+const int Graph<Type> :: MAXIMUM;
+
+template <class Type>
+Graph<Type> :: Graph()
+{
+    this->vertexCount = 0;
+}
+
+template <class Type>
+int Graph<Type> :: size() const
+{
+    return vertexCount;
+}
+template <class Type>
+Type& Graph<Type> :: operator[](int vertex)
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+
+template <class Type>
+Type Graph<Type> :: operator[](int vertex) const
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+template <class Type>
+void Graph<Type> :: addVertex(const Type& value)
+{
+    assert(vertexCount < MAXIMUM);
+    int newVertexNumber = vertexCount;
+    vertexCount++;
+    for(int otherVertexNumber = 0; otherVertexNumber < vertexCount; otherVertexNumber++)
+    {
+        adjacencyMatrix[otherVertexNumber][newVertexNumber] = false;
+        adjacencyMatrix[newVertexNumber][otherVertexNumber] = false;
+    }
+    graphData[newVertexNumber] = value;
+}
+template <class Type>
+void Graph<Type> :: addEdge(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = true;
+}
+template <class Type>
+void Graph<Type> :: addEdgeCost(int source, int target, int cost)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    weightCostMatrix[source][target] = cost;
+    weightCostMatrix[target][source] = cost;
+}
+template <class Type>
+void Graph<Type> :: addEdgeUndirected(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = true;
+    adjacencyMatrix[target][source] = true;bi
+    
+}
+
+#endif /* Graph_h */
