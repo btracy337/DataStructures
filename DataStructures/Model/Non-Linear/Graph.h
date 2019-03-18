13e6f524e5a5331580b40a204502ebde9817cfb3
Bryan Tracy <btracy337@gmail.com>
2019-03-18 08:29:54 -0600

    Finished the Graph class

--- DataStructures/Model/Non-Linear/Graph.h
+++ DataStructures/Model/Non-Linear/Graph.h
@@ -213,6 +213,38 @@ void Graph<Type> :: breadthFirstTraversal(Graph<Type> & currentGraph, int vertex
             
     }
 }
+template <class Type>
+int Graph<Type> :: costTraversal(Graph<Type> & currentGraph, int start)
+{
+    assert(start >= 0 && start < vertexCount);
+    int cost = 0;
+    bool visited[MAXIMUM];
+    std::set<int> connections;
+    std::set<int>::iterator setIterator;
+    std::queue<int> vertexQueue;
+    
+    std::fill_n(visited, currentGraph.size(), false);
+    visited[start] = true;
+    
+    vertexQueue.push(start);
+    while(!vertexQueue.empty())
+    {
+        int currentIndex = vertexQueue.front();
+        connections = currentGraph.neighbors(currentIndex);
+        vertexQueue.pop();
+        
+        for(setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
+        {
+            if(!visited[*setIterator])
+            {
+                cost+= weightCostMatrix[currentIndex][*setIterator];
+                visited[*setIterator] = true;
+                vertexQueue.push(*setIterator);
+            }
+        }
+    }
+    return cost;
+}
 
 
 #endif /* Graph_h */
